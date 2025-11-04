af43bd6ff40452968dc7d74ca5117a43d4d51f65
Tony <anthonyaldazg@gmail.com>
2025-11-04 12:06:20 -0500

    Delete eww directory

--- eww/scripts/prueba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-# Crear la ventana de kitty con la clase "QR" y el título "PA"
-kitten @ launch --type os-window --os-window-class QR --title PA
-# Esperar un momento para asegurarse de que la ventana se haya creado
-sleep 1
-# Enviar el comando a la ventana
-# \n es igual a dar un enter, de forma q ejecuta el comando
-kitten @ send-text --exclude-active -m title:PA "nmcli dev wifi show\n"
